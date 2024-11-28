548eb9428bd1112b4922961159dfa25a61b73001
MichaelR <spider747@gmail.com>
2024-11-28 23:52:02 +0200

    Add tests for user get and registration

---
+++ tests/test_user_register.py
@@ -0,0 +1,33 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from datetime import datetime
+
+class TestUserRegister(BaseCase):
+    def setup_method(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%e%m%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_create_user_successfully(self):
+        data = self.prepare_registration_data()
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        assert response.status_code == 200, f"Unexpected status code {response.status_code}"
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+        print(response.content)
+
+    def test_create_user_with_existing_mail(self):
+        email = 'vinkotov@example.com'
+        data = self.prepare_registration_data(email)
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("UTF-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+        print(response.status_code)
+        print(response.content)
